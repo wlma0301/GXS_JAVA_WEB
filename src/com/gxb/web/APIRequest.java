d38bffb62038f6477cf01144c99112654b8bd762
jjwang <jjwang@jjwang-PC>
2017-12-11 12:05:23 +0800

    删除无用包

--- src/com/gxb/web/APIRequest.java
+++ src/com/gxb/web/APIRequest.java
@@ -9,7 +9,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
-import java.net.URL;
 import java.util.Properties;
 
 import com.gxb.api.APIObj;
